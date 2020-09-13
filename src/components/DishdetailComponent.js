cdeaa1a2dec6d47b4f1465586d4e0bde0f16e00d
jgatto1 <jgatto@correo.um.edu.uy>
2020-09-13 17:56:23 -0300

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
-import {Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle, Navbar, NavbarBrand} from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem
+} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 function RenderDish({dish}) {
     if (dish) {
@@ -45,8 +49,26 @@ const RenderComments = ({comments}) => {
 
 const DishDetail = (props) => {
     return (
-        <RenderDish dish={props.dish}/>
-        // renderDish(props.dish)
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish}/>
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments}/>
+                </div>
+            </div>
+        </div>
     );
 }
 
